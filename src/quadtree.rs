7af8adee48ed5d243008f9993cce7529bf94add9
Martin Tomasi <martin.tomasi@gmail.com>
2015-02-18 12:43:33 +0100

    more work on quadtree

--- src/quadtree.rs
+++ src/quadtree.rs
@@ -9,6 +9,7 @@ pub trait HasCoordinates: Clone {
     fn coords(&self) -> Point2<u32>;
 }
 
+#[derive(Debug)]
 pub struct QuadTree<T> {
     root: QuadTreeNode<T>,
     size: usize
@@ -20,7 +21,8 @@ impl<T: HasCoordinates> QuadTree<T> {
             root: QuadTreeNode {
                 values: Vec::with_capacity(NODE_CAPACITY),
                 bounds: bounds,
-                children: Box::new(None)
+                children: Box::new(None),
+                level: 0
             },
             // Empty nodes don't count for size
             size: 0
@@ -30,26 +32,43 @@ impl<T: HasCoordinates> QuadTree<T> {
     pub fn push(&mut self, value: T) {
         let success = self.root.insert(value);
         if !success { panic!("Could not insert value into tree!"); }
+        self.size += 1;
     }
 }
 
+#[derive(Debug)]
 struct QuadTreeNode<T> {
     values: Vec<T>,
     bounds: Bounds,
-    children: Box<Option<[QuadTreeNode<T>; 4]>>
+    children: Box<Option<[QuadTreeNode<T>; 4]>>,
+    level: usize
 }
 
 impl<T: HasCoordinates> QuadTreeNode<T> {
-    fn new(values: Vec<T>, bounds: Bounds, children: Option<[QuadTreeNode<T>; 4]>) -> QuadTreeNode<T> {
+    fn new(values: Vec<T>, bounds: Bounds, children: Option<[QuadTreeNode<T>; 4]>, level: usize) -> QuadTreeNode<T> {
         QuadTreeNode {
             values: values,
             bounds: bounds,
-            children: Box::new(children)
+            children: Box::new(children),
+            level: level
         }
     }
 
     fn subdivide(&mut self) {
-        // TODO
+        let x_size = self.bounds.dim().x / 2;
+        let y_size = self.bounds.dim().y / 2;
+
+        let x = self.bounds.min.x;
+        let y = self.bounds.min.y;
+        
+        let ch = Some([
+            QuadTreeNode::new(Vec::new(), Aabb2::new(Point2::new(x, y), Point2::new(x + x_size, y + y_size)), None, self.level + 1),
+            QuadTreeNode::new(Vec::new(), Aabb2::new(Point2::new(x + x_size, y), Point2::new(x + x_size * 2, y + y_size)), None, self.level + 1),
+            QuadTreeNode::new(Vec::new(), Aabb2::new(Point2::new(x, y + y_size), Point2::new(x + x_size, y + y_size * 2)), None, self.level + 1),
+            QuadTreeNode::new(Vec::new(), Aabb2::new(Point2::new(x + x_size, y + y_size), Point2::new(x + x_size * 2, y + y_size * 2)), None, self.level + 1),
+            ]);
+        
+        self.children = Box::new(ch);
     }
 
     fn insert(&mut self, value: T) -> bool {
@@ -66,12 +85,30 @@ impl<T: HasCoordinates> QuadTreeNode<T> {
             self.subdivide();
         }
 
-        // for c in self.children.unwrap().iter_mut() {
-        //     if c.insert(value.clone()) {
-        //         return true;
-        //     }
-        // }
-        // assert!(false);
-        false
+        for c in self.children.as_mut().unwrap().iter_mut() {
+            if c.insert(value.clone()) {
+                return true;
+            }
+        }
+        panic!("Insertion failed for unknown reason!")
+    }
+}
+
+mod tests {
+    use super::*;
+    use cgmath::{Point2, Aabb2};
+
+    #[derive(Debug, Clone)]
+    struct Value { p: Point2<u32> }
+    impl HasCoordinates for Value {
+        fn coords(&self) -> Point2<u32> {
+            self.p
+        }
+    }
+    
+    #[test]
+    fn insert_values() {
+        //let mut tree: QuadTree<Value> = QuadTree::new(Aabb2::new(Point2::new(0, 0), Point2::new(100, 100)));
+        
     }
 }
